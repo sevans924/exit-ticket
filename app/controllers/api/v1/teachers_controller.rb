80ca9c43a5327e8cdff52aa6926ad7f616fea027
Sarah Elzinga <sevans924@gmail.com>
2019-05-28 11:59:47 -0500

    began to work on controllers - still needs work

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -1,2 +1,39 @@
 class TeachersController < ApplicationController
-end
+  before_action :find_teacher, only: [:update, :destroy]
+
+      def index
+        @no = Note.all
+        render json: @notes
+      end
+
+      def create
+        @note = Note.new(note_params)
+        if @note.save
+          render json: @note , status: :accepted
+        else
+          render json: { errors: @note.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+
+      def update
+        @note.update(note_params)
+        if @note.save
+          render json: @note, status: :accepted
+        else
+          render json: { errors: @note.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+
+
+##################
+private
+
+      def teacher_params
+        params.require(:note).permit(:first_name, :last_name, :email)
+      end
+
+      def find_teacher
+        @teacher = Teacher.find(params[:id])
+      end
+
+  end
