f3949fee20fa58653c76540d31f9f13293dff0ba
Sarah Elzinga <sevans924@gmail.com>
2019-05-28 11:33:59 -0500

    added notes model and migration

---
+++ db/migrate/20190528163244_create_notes.rb
@@ -0,0 +1,14 @@
+class CreateNotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notes do |t|
+      t.string :topic
+      t.text :content
+      t.string :prompt
+      t.string :time
+      t.references :student, foreign_key: true
+      t.references :teacher, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
