9cba7e55afb9359ab4760141c2d01f3b648e4d81
Sarah Elzinga <sevans924@gmail.com>
2019-05-28 13:42:13 -0500

    deleted duplicates

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -2,27 +2,11 @@ class TeachersController < ApplicationController
   before_action :find_teacher, only: [:update, :destroy]
 
       def index
-        @no = Note.all
+        @note = Note.all
         render json: @notes
       end
 
-      def create
-        @note = Note.new(note_params)
-        if @note.save
-          render json: @note , status: :accepted
-        else
-          render json: { errors: @note.errors.full_messages }, status: :unprocessible_entity
-        end
-      end
 
-      def update
-        @note.update(note_params)
-        if @note.save
-          render json: @note, status: :accepted
-        else
-          render json: { errors: @note.errors.full_messages }, status: :unprocessible_entity
-        end
-      end
 
 
 ##################
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,24 @@ Rails.application.routes.draw do
 
   namespace :api do
      namespace :v1 do
-       resources :notes, :teachers, :students
+       resources :notes
      end
    end
+
+   namespace :api do
+      namespace :v1 do
+        resources :teachers, only: [:index, :show]
+      end
+    end
+
+    namespace :api do
+       namespace :v1 do
+         resources :students, only: [:index, :show]
+       end
+     end
+
+
+
 
 
 end
